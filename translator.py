4d47924fbc43fe18dfbef2799502d1027203d857
Jeffrey Pan <jeffreypann@gmail.com>
2021-04-14 17:44:06 -0400

    updated datetime translation and added more option fields

--- translator.py
+++ translator.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from datetime import timedelta
 
 
 # player single stat selection
@@ -6,35 +8,53 @@
 
 # "SELECT (stat) FROM Atbats INNER JOIN Games ON gameid INNER JOIN PlayerNames ON playerId WHERE  dateTime"
 
-# -dr date range 
-def dr_option(start,end):
+def dt_date_converter(date):
+    return datetime.strptime(date, '%d/%m/%Y')
+
+# input is datetime objects
+def dr_dt(start,end):
     return "dateTime > {start} AND dateTime < {end}".format(start=start,end=end)
 
 # -d exact
 def d_option(date):
-    return "dateTime = {date}".format(date=date)
+    dt = dt_date_converter(date)
+    return "dateTime = {date}".format(date=dt)
+
+# -dr date range 
+def dr_option(start,end):
+    start_dt = dt_date_converter(start)
+    end_dt = dt_date_converter(end)
+    return dr_dt(start_dt,end_dt)
+
+# -s season
+def season_option(s):
+    start = "01/01/" + s
+    start_dt = dt_date_converter(start)
+    end_dt = start_dt + timedelta(days=365) 
+    return dr_dt(start_dt,end_dt)
 
 # -e event (flyout, strikout..)
 def event_option(e):
     return "event = {event}".format(event=e)
 
-def playerID_option(id):
-    return "playerID = {id}".format(id=id)
+def stat_count_option(stat, count):
+    return "{stat} = {count}".format(stat=stat, count=count)
 
 def player_name_option(first,last):
     return "firstName = {first} AND lastName = {last}"
+
+
 
 # TODO finish off adding player options
 
 
 # single player stat
-def select_stat_statement(stat, get_min=False, get_max=False, get_count=False):
+def select_player_stat(stat, get_min=False, get_max=False):
     q = "SELECT "
 
     if get_min: q += "MIN({stat})".format(stat=stat)
     elif get_max: q += "MAX({stat})".format(stat=stat)
-    elif get_count: q += "COUNT({stat})".format(stat=stat)
-    else: q += stat
+    else: q += "COUNT({stat})".format(stat=stat)
 
     q += " FROM "
 
@@ -44,8 +64,10 @@ def select_stat_statement(stat, get_min=False, get_max=False, get_count=False):
 # TODO add team stats query functionality
 
 
-def from_statement():
+def from_statement(options):
     #TODO classify diff types and figure out joins
+    if "batter" in options:
+        return "FROM AtBats INNER JOIN Game INNER JOIN PlayerName"
     return ""
 
 def where_statement(options):
@@ -59,3 +81,5 @@ def query_builder(stat, options):
     query += from_statement()
     query += where_statement()
     return query
+
+#print(select_player_stat("event") + from_statement() +  + event_option("Strikeout"))
